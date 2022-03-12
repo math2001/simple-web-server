cd650001a29d2d4b853b1a4712456d4c8735c391
Mathieu PATUREL <australie.p@gmail.com>
2022-03-13 09:44:32 +1100

    try to use nohup

--- start_server.bash
+++ start_server.bash
@@ -3,8 +3,6 @@ echo "Killing server"
 # if server is already running, kill, wait and print status (should not find the process)
 [ -f ~/server.pid ] && kill $(cat ~/server.pid) && sleep 5 && ! ps -fp $(cat ~/server.pid)
 echo "Starting server"
-~/.local/bin/waitress-serve server:app &   # start in the background
+nohup ~/.local/bin/waitress-serve server:app &   # start in the background
 echo $! > ~/server.pid
 echo "saved PID"
-disown
-echo "done"
