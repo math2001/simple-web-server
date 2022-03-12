79befd821fad0e3baef98c64ecec18b9cfea9a89
Mathieu PATUREL <australie.p@gmail.com>
2022-03-13 09:11:00 +1100

    add deploy server code

---
+++ start_server.bash
@@ -0,0 +1,7 @@
+echo "Killing server"
+# if server is already running, kill, wait and print status (should not find the process)
+[ -f ~/server.pid ] && kill $(cat ~/server.pid) && sleep 5 && ps -fp $(cat ~/server.pid)
+echo "Starting server"
+~/.local/bin/waitress-serve server:app &   # start in the background
+echo $! > ~/server.pid
+disown
