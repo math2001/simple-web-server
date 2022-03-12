8b518c73d92c27fef85527bb46eba544c52e0bf3
Mathieu PATUREL <australie.p@gmail.com>
2022-03-13 09:13:55 +1100

    don't fail if repo is already cloned

--- start_server.bash
+++ start_server.bash
@@ -1,6 +1,7 @@
+set -e
 echo "Killing server"
 # if server is already running, kill, wait and print status (should not find the process)
-[ -f ~/server.pid ] && kill $(cat ~/server.pid) && sleep 5 && ps -fp $(cat ~/server.pid)
+[ -f ~/server.pid ] && kill $(cat ~/server.pid) && sleep 5 && ! ps -fp $(cat ~/server.pid)
 echo "Starting server"
 ~/.local/bin/waitress-serve server:app &   # start in the background
 echo $! > ~/server.pid
